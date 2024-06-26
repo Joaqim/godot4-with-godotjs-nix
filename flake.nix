421ab5cc5f728731847da2b9efc492bfa11b8b34
Sergey Bubnov <omgbebebe@gmail.com>
2024-06-26 15:15:56 +0400

    retab

--- flake.nix
+++ flake.nix
@@ -11,27 +11,27 @@
       imports = [];
       systems = [ "x86_64-linux" "aarch64-linux" ];
       perSystem = { config, self', inputs', pkgs, system, ... }:
-				let
-					godot = pkgs.godot_4.overrideAttrs (p: f: rec {
-					  version = "4.3.0beta2";
-						commitHash = "95110ddcb41ba4b9b1f8c9bf58c8910f6616e60a";
+        let
+          godot = pkgs.godot_4.overrideAttrs (p: f: rec {
+            version = "4.3.0beta2";
+            commitHash = "95110ddcb41ba4b9b1f8c9bf58c8910f6616e60a";
 
-						src = pkgs.fetchFromGitHub {
-						  owner = "godotengine";
-							repo = "godot";
-							rev = commitHash;
-							sha256 = "sha256-j8xQI4vRkTtohkva5+C8dXVg+/cIAPOe1IsVJdW6THY="; #pkgs.lib.fakeSha256;
-						};
-					});
+            src = pkgs.fetchFromGitHub {
+              owner = "godotengine";
+              repo = "godot";
+              rev = commitHash;
+              sha256 = "sha256-j8xQI4vRkTtohkva5+C8dXVg+/cIAPOe1IsVJdW6THY="; #pkgs.lib.fakeSha256;
+            };
+          });
          in { 
-				 	packages = rec {
-						release = pkgs.callPackage godot { withTarget = "template_release"; };
-						debug = pkgs.callPackage godot { withTarget = "template_debug"; };
-						editor = pkgs.callPackage godot { withTarget = "editro"; };
-          	default = editor;
-        	};
-        	devShells.default = config.packages.editor;
-				};
+          packages = rec {
+            release = pkgs.callPackage godot { withTarget = "template_release"; };
+            debug = pkgs.callPackage godot { withTarget = "template_debug"; };
+            editor = pkgs.callPackage godot { withTarget = "editor"; };
+            default = editor;
+          };
+          devShells.default = config.packages.editor;
+        };
       flake = {
       };
     };
