25006608ea5bfd28bd5bcd56128f153e88778eb1
Sergey Bubnov <omgbebebe@gmail.com>
2024-06-27 23:38:15 +0400

    add template_{debug,release} packages to build a standalone apps

--- flake.nix
+++ flake.nix
@@ -12,7 +12,8 @@
       systems = [ "x86_64-linux" "aarch64-linux" ];
       perSystem = { config, self', inputs', pkgs, system, ... }:
         let
-          godot = pkgs.godot_4.overrideAttrs (old: rec {
+#          godot = pkgs.godot_4.overrideAttrs (old: rec {
+          godot = pkgs.godot_4.overrideAttrs rec {
             version = "4.3.0beta2";
             commitHash = "95110ddcb41ba4b9b1f8c9bf58c8910f6616e60a";
 
@@ -22,10 +23,21 @@
               rev = commitHash;
               sha256 = "sha256-j8xQI4vRkTtohkva5+C8dXVg+/cIAPOe1IsVJdW6THY="; #pkgs.lib.fakeSha256;
             };
-          });
+            outputs = [ "out" ];
+            installPhase = ''
+              mkdir -p "$out/bin"
+              cp bin/godot.* $out/bin/
+            '';
+          };
+          godot_template_debug = godot.override {withTarget="template_debug";};
+          godot_template_release = godot.override {withTarget="template_release";};
+          godot_editor = godot.override {withTarget="editor";};
          in { 
           packages = rec {
-            default = godot;
+            template_release = godot_template_release;
+            template_debug = godot_template_debug;
+            editor = godot_editor;
+            default = editor;
           };
           devShells.default = config.packages.default;
         };
